560a8b92a5ff2ab1f581030d3ee9c31911c57d5f
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-08-23 23:05:36 -0700

    a

--- client/src/components/PortfolioInstagram/index.js
+++ client/src/components/PortfolioInstagram/index.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function PortfolioInstagram() {
   return (
-    <div>
-      {/* <div id="Instagram" className="elfsight-app-271bb46e-772f-49ef-852e-efefa1cfee68 uk-animation-fade"></div> */}
+    <div id="Instagram">
+      <div className="elfsight-app-271bb46e-772f-49ef-852e-efefa1cfee68 uk-animation-fade"></div>
     </div>
   );
 }
