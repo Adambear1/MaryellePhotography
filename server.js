66849ab819987bcf0bb237d6b37c06535f18dbdc
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-08-27 19:42:59 -0700

    server update

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const helmet = require("helmet");
 const bodyParser = require("body-parser");
+const db = require("./config");
 const logger = require("morgan");
+const cors = require("cors");
 const path = require("path");
+const expressSession = require("express-session");
+const MongoStore = require("connect-mongo")(expressSession);
 
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
@@ -13,8 +17,20 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger("dev"));
+app.use(cors());
 
 // DB => API
+db().then((connection) => {
+  app.use(
+    expressSession({
+      secret: process.env.SECRET,
+      resave: true,
+      saveUninitialized: true,
+      cookie: { secure: true },
+      store: new MongoStore({ mongooseConnection: connection }),
+    })
+  );
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
